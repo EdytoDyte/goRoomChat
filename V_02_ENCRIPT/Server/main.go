5b809cab1985a936ffe618ff1987d00aaa91e547
Jesus <107671943+EdytoDyte@users.noreply.github.com>
2023-02-03 13:35:45 +0100

    Comments and Debug logs, translated to enlgish

--- V_02_ENCRIPT/Server/main.go
+++ V_02_ENCRIPT/Server/main.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 )
 
-// Esta estructura contendra el nombre de la sala,los clientes, las publick keys de los clientes, la privatekey y la publickkey de la sala
+// This structure will contain the name of the room, the clients, the clients' public keys, the private key and the room's public key.
 type room struct {
 	nombre     string
 	clientes   []net.Conn
@@ -21,29 +21,30 @@ type room struct {
 	publickKey *rsa.PublicKey
 }
 
-// Utilizamos esta estructura para poder recibir las claves
+// We use this structure to receive the keys.
 type keys struct {
 	Publick []byte
 }
 
-// Utilizamos esta estructura para poder recibir mensajes encriptados
+// We use this structure to receive encrypted messages.
 type msges struct {
 	Mensaje []byte
 }
 
-// Array para poder almacenar las salas de chat
+// Array to store chat rooms.
+
 var rooms []room
 
 func main() {
-	fmt.Println("Escuchando desde el servidor en el puerto 8080") // Mensaje descriptivo
-	conexion, err := net.Listen("tcp", ":8080")                   // Empezamos a esuchar por tcp en el peurto 8080
+	fmt.Println("Listening on the port: 8080")  // Descriptive message.
+	conexion, err := net.Listen("tcp", ":8080") // We start listening over TCP on port 8080.
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer conexion.Close()
-	conns := make(map[net.Conn]string) // Sirve para almacenar datos de la conexion para poder usarlos despues
-	// Si se acepta la conexion se ira a la funcion para gestionar las conexiones si hay un error lo indicara
+	conns := make(map[net.Conn]string) //It serves to store connection data so that it can be used later.
+	// If the connection is accepted, it will go to the function to manage connections, if there is an error, it will indicate it.
 	for {
 		conn, err := conexion.Accept()
 		if err != nil {
@@ -55,105 +56,105 @@ func main() {
 }
 func handleConnection(conexion net.Conn, conexiones map[net.Conn]string) {
 	defer conexion.Close()
-	hash, _ := bufio.NewReader(conexion).ReadString('\n') // Lector de claves publicas
+	hash, _ := bufio.NewReader(conexion).ReadString('\n') // Public key reader
 	var Clavese keys
-	err := json.Unmarshal([]byte(hash), &Clavese) // Parsear para poder leer el json
+	err := json.Unmarshal([]byte(hash), &Clavese) // Parse to read the json
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	pubkey, _ := x509.ParsePKCS1PublicKey(Clavese.Publick)  // Obtener la publickey de la conexion para poder usarla despues
-	opcion, _ := bufio.NewReader(conexion).ReadString('\n') // Lector de opcion
+	pubkey, _ := x509.ParsePKCS1PublicKey(Clavese.Publick)  // Get the connection's public key to use later
+	opcion, _ := bufio.NewReader(conexion).ReadString('\n') // Option reader
 	conexiones[conexion] = opcion
-	fmt.Print("Se ha selecionado la opcion: " + opcion)
-	nombreSala, _ := bufio.NewReader(conexion).ReadString('\n') // Lector del nombre de sala
+	fmt.Print("Te selected options is : " + opcion)
+	nombreSala, _ := bufio.NewReader(conexion).ReadString('\n') // Room name reader
 	conexiones[conexion] = nombreSala
-	fmt.Print("Nombre de sala: " + nombreSala)
+	fmt.Print("Name of the room: " + nombreSala)
 	joinRoom(conexion, nombreSala, pubkey)
-	username, _ := bufio.NewReader(conexion).ReadString('\n') // Lector de usuarios
+	username, _ := bufio.NewReader(conexion).ReadString('\n') // User reader
 	conexiones[conexion] = username
-	fmt.Print("Se ha unido al chat  " + username)
+	fmt.Print("The user has entered to the room:" + username)
 	for {
-		mensajes, err := bufio.NewReader(conexion).ReadString('\n') // Lector de mensajes
-		var message msges                                           // Inicializar una variable que sera el contenedor de json
-		err2 := json.Unmarshal([]byte(mensajes), &message)          // Parsear el json para poder leerlos
+		mensajes, err := bufio.NewReader(conexion).ReadString('\n') // Message reader
+		var message msges                                           // Initialize a variable that will be the json container
+		err2 := json.Unmarshal([]byte(mensajes), &message)          // Parse the json to be able to read it
 		if err2 != nil {
 			fmt.Println(err)
 			return
 		}
-		// Saber la privateKey de la room
-		var r *room // puntero a la estructura room
+		// Know the room's private key
+		var r *room // Pointer to the room structure
 		for i := range rooms {
 			if rooms[i].nombre == nombreSala {
-				r = &rooms[i] // Referencia a la sala que vamos a usar
+				r = &rooms[i] // Reference to the room we are using
 				break
 			}
 		}
-		abc := desencriptar(message.Mensaje, r.privateKey) // Desencriptar con la privatekey de la sala
+		fmt.Print(r.nombre)
+		abc := desencriptar(message.Mensaje, r.privateKey) // Decrypt with the room's private key
 		fmt.Print(string(abc) + "\n")
 		if err != nil {
-			fmt.Println("Conexión cerrada")
+			fmt.Println("Connection closed")
 			return
 		}
-		user := strings.TrimSpace(conexiones[conexion])  // Limpiar espacios en blanco del usuario
-		msg := string(user + " : " + string(abc) + "\n") // Formatear texto
-		fmt.Print(msg)                                   // Mostrar por pantalla
-		broadcast(msg, nombreSala)                       // Enviar a todos los de la sala
+		user := strings.TrimSpace(conexiones[conexion])  // Clear whitespaces from the user
+		msg := string(user + " : " + string(abc) + "\n") // Format text
+		fmt.Print(msg)                                   // Display on screen
+		broadcast(msg, nombreSala)                       // Send to all in the room
 	}
 }
 
-// Esta funcion tratara de crear la sala en el caso de que no exista y en el que si pues añadira al usuario
+// This function will try to create the room in the case it doesn't exist and if it does, it will add the user.
 func joinRoom(conexion net.Conn, salaName string, pubkiclkey *rsa.PublicKey) {
-	// Le pasamos un parametro del nombre de la sala
-	var r *room            // puntero a la estructura room
-	for i := range rooms { // Bucle de las salas
-		if rooms[i].nombre == salaName { // Busca la sala que tiene el nombre pasado por la funcioon
-			fmt.Println("Se ha añadido a la sala " + salaName)
-			r = &rooms[i]                                        // Se hace un referencia a la sala
-			pemKey, _ := x509.MarshalPKIXPublicKey(r.publickKey) // La clave publicka se parsea
-			keyss := keys{                                       // Objeto Json
+	// We pass a parameter of the room name
+	var r *room            // Pointer to the room structure
+	for i := range rooms { // Rooms loop
+		if rooms[i].nombre == salaName { // Searches for the room with the name passed by the function
+			fmt.Println("The user has adde to the room - " + salaName)
+			r = &rooms[i]                                        // A reference is made to the room
+			pemKey, _ := x509.MarshalPKIXPublicKey(r.publickKey) // The public key is parsed
+			keyss := keys{                                       // Json object
 				Publick: pemKey,
 			}
-			public, _ := json.Marshal(keyss) // Se transforma en objeto json y se parsea a el
-			conexion.Write(public)           // Se le manda al cliente para poder obtener la clave publica del servidor
+			public, _ := json.Marshal(keyss) // It is transformed into a json object and parsed
+			conexion.Write(public)           // It is sent to the client to obtain the server's public key
 			conexion.Write([]byte("\n"))
 			break
 		}
 	}
-	// Si no encuentra la sala, la creara
+	// If it doesn't find the room, it will create it
 	if r == nil {
-		fmt.Println("Se ha creado la sala " + salaName)
-		p1, p2 := getHash(conexion)                                 // Obtenemos la private key y la publick key de la sala
-		r = &room{nombre: salaName, privateKey: p1, publickKey: p2} // Creamos la sala con los parametros que hemos obtenido
-		rooms = append(rooms, *r)                                   // La añadimos a la array de salas
-		joinRoom(conexion, salaName, pubkiclkey)                    // Utilizamos esta funcion para que el cliente se pueda unir a la sala despues de crearla
+		fmt.Println("The room has been created: " + salaName)
+		p1, p2 := getHash(conexion)                                 // We get the room's private key and public key
+		r = &room{nombre: salaName, privateKey: p1, publickKey: p2} // We create the room with the parameters we have obtained
+		rooms = append(rooms, *r)                                   // We add it to the rooms array
+		joinRoom(conexion, salaName, pubkiclkey)                    // We use this function for the client to be able to join the room after creating it
 	}
-	r.clientes = append(r.clientes, conexion) // Añadimos las conexiones a la sala en que estamos
-	r.keys = append(r.keys, pubkiclkey)       // Añadimos las publick keys a la sala en que estamos
+	r.clientes = append(r.clientes, conexion) // We add the connections to the room we are in
+	r.keys = append(r.keys, pubkiclkey)       // We add the public keys to the room we are in
 }
 
-// Envia un mensaje a todos los usuarios de la sala que se ha pasado como parametro
+// Sends a message to all users in the room passed as a parameter
 func broadcast(mensaje string, nombreSala string) {
-	for i := range rooms { // busca en la array de salas
-		if rooms[i].nombre == nombreSala { // Comprueba en que sala estamos
-			for j := range rooms[i].clientes { // Se obtiene cada cliente
+	for i := range rooms { // Searches in the rooms array
+		if rooms[i].nombre == nombreSala { // Checks in which room we are
+			for j := range rooms[i].clientes { // Gets each client
 				msgEncript := encriptar([]byte(mensaje), rooms[i].keys[j])
 				message := msges{
 					Mensaje: msgEncript,
 				}
 				msgJson, _ := json.Marshal(message)
-				rooms[i].clientes[j].Write(msgJson)      // Se envia el mensaje para cada cliente
-				rooms[i].clientes[j].Write([]byte("\n")) // Se envia el mensaje para cada cliente
-
+				rooms[i].clientes[j].Write(msgJson) // Sends the message for each client
+				rooms[i].clientes[j].Write([]byte("\n"))
 			}
 			break
 		}
 	}
 }
 
-// Encripta un mensaje que haya sido pasado por byte con la publica del destino
+// Encrypts a message passed by byte with the destination's public
 func encriptar(msg []byte, publicKey *rsa.PublicKey) []byte {
-	mensaje, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, publicKey, msg, nil) // Crea la encriptacion
+	mensaje, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, publicKey, msg, nil) // Creates the encryption
 	if err != nil {
 		fmt.Println(err)
 		return []byte("")
@@ -161,18 +162,18 @@ func encriptar(msg []byte, publicKey *rsa.PublicKey) []byte {
 	return mensaje
 }
 
-// Desencripta un mensaje que haya sido pasado por byte con la privada del servidor
+// Decrypts a message passed by byte with the server's private
 func desencriptar(msg []byte, privateKey *rsa.PrivateKey) []byte {
-	mensaje, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, privateKey, msg, nil) // Desencripta
+	mensaje, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, privateKey, msg, nil) // Decrypts
 	if err != nil {
 		fmt.Println(err)
 		return []byte("")
 	}
-	return mensaje // Devuelve el mensaje desencriptado
+	return mensaje // Returns the decrypted message
 }
 func getHash(conn net.Conn) (*rsa.PrivateKey, *rsa.PublicKey) {
-	fmt.Println("::: Enviando c. de seguridad :::")
-	// Generate a new RSA key pair
+	fmt.Println("::: Sending security key :::")
+	// Generate a new RSA key pair.
 	privatekey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		fmt.Println(err)
